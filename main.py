5952dc84009da4a3d7daa29e66e9cd8b8cf44f71
Dhruv-Dugar <89693919+Dhruv-Dugar@users.noreply.github.com>
2022-02-25 22:50:10 +0530

    changed library from spotipy to tekore due to authentication bugs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import tekore as tk
 from dotenv import load_dotenv
 
 # using .env to store the credentials, and then storing in a dictionary for easy access
@@ -9,20 +8,13 @@ load_dotenv()
 credentials = {
     "client_id": os.getenv("CLIENT_ID"),
     "client_secret": os.getenv("CLIENT_SECRET"),
+    "redirect_uri": os.getenv("REDIRECT_URI"),
 }
 
+conf = (credentials["client_id"], credentials["client_secret"], credentials["redirect_uri"])
 
-# Led Zeppelin spotify url
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
 
-# authenticate with the spotify API using our tokens and secret
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=credentials["client_id"],
-                                                                              client_secret=credentials[
-                                                                                  "client_secret"]))
-results = spotify.artist_top_tracks(lz_uri)
-
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
+spotify = tk.Spotify(token)
+tracks = spotify.current_user_top_tracks(limit=10)
+spotify.playback_start_tracks([t.id for t in tracks.items])
